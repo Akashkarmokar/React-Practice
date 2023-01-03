a2d2f4cd328929ee0d0fe4d899ffa93721815084
Akash <akashcsemu@gmail.com>
2023-01-03 22:44:04 +0600

    feat: Context API Learn how to use consumer data (context) in functional component with the help of useContext hook (built in hook)

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import { useContext } from 'react';
 import themeContext from '../Contexts/themeContext';
 import Counter from './Counter';
 import HoverCounter from './HoverCounter';
 
-export default class Content extends React.Component {
-    render() {
-        const { theme, switchTheme } = this.context;
-        return (
-            <div>
-                <h2>This a content section</h2>
-                <Counter
-                    renderLogic={(counter, handleCounter) => (
-                        <HoverCounter
-                            counter={counter}
-                            handleCounter={handleCounter}
-                            theme={theme}
-                            switchTheme={switchTheme}
-                        />
-                    )}
-                />
-            </div>
-        );
-    }
+export default function Content() {
+    const context = useContext(themeContext);
+    const { theme, switchTheme } = context;
+    return (
+        <div>
+            <h2>This a content section</h2>
+            <Counter
+                renderLogic={(counter, handleCounter) => (
+                    <HoverCounter
+                        counter={counter}
+                        handleCounter={handleCounter}
+                        theme={theme}
+                        switchTheme={switchTheme}
+                    />
+                )}
+            />
+        </div>
+    );
 }
-
-Content.contextType = themeContext;
